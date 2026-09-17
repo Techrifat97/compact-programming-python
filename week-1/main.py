7eaba63ad35a3d68c48a359eb059a004f7a19941
Rifat Ali Shaon <rifat-mac@Rifats-MacBook-Pro.local>
2026-09-17 15:29:37 +0600

    Complete Week 1 Task 1 circle area

---
+++ week-1/main.py
@@ -0,0 +1,7 @@
+from math import pi
+
+if __name__ == '__main__':
+    radius = float(input(" input the radius of a circle: "))
+    area_of_a_circle = pi * (radius**2)
+    print(f"The area of circle with radius {radius} is : {area_of_a_circle}")
+
