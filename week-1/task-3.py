667204f4a6b4db6f663d474b5cb2a74a003c4d8b
Rifat Ali Shaon <rifatshaon.tech@gmail.com>
2026-09-18 19:22:33 +0600

    Complete week 1 task 3

---
+++ week-1/task-3.py
@@ -0,0 +1,24 @@
+# Integer to Float
+integer_number = 10
+float_number = float(integer_number)
+print("Integer to Float:", float_number)
+
+# Float to Integer
+decimal_number = 10.5
+integer_from_float = int(decimal_number)
+print("Float to Integer:", integer_from_float)
+
+# Integer to String
+number = 25
+number_as_string = str(number)
+print("Integer to String:", number_as_string)
+
+# Numeric String to Integer
+numeric_string = "50"
+string_as_integer = int(numeric_string)
+print("Numeric String to Integer:", string_as_integer)
+
+# Integer to Boolean
+integer_value = 1
+boolean_value = bool(integer_value)
+print("Integer to Boolean:", boolean_value)
