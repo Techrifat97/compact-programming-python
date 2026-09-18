df7e9e9cd2e7726af40bf703bbd9208b48fbafa0
Rifat Ali Shaon <rifatshaon.tech@gmail.com>
2026-09-18 19:19:31 +0600

    Complete Week 1 Task 2

---
+++ week-1/task-2.py
@@ -0,0 +1,16 @@
+zahl = 10
+kommazahl = 10.5
+text = "Hello, World!"
+wahrheitswert = True
+
+print("Integer:", zahl)
+print("Type:", type(zahl))
+
+print("Float:", kommazahl)
+print("Type:", type(kommazahl))
+
+print("String:", text)
+print("Type:", type(text))
+
+print("Boolean:", wahrheitswert)
+print("Type:", type(wahrheitswert))
